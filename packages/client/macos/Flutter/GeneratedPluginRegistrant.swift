4e034f039d0c5df97e7ec64106f0cd09366e2cd8
Kiri <cultivatingghoul@gmail.com>
2026-05-22 10:57:42 +0200

    added extra packages  for voice, persistance and editing

--- packages/client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
+import flutter_tts
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
