1c3564d18fe4dff4b02d057f52f98f5c195ac5fb
Kiri <cultivatingghoul@gmail.com>
2026-05-23 14:22:01 +0200

    setup logging and added drift packages

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -2,9 +2,21 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:fluentepub/config/router.dart';
 import 'package:fluentepub/config/theme/dark_theme.dart';
 import 'package:fluentepub/config/theme/light_theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'dart:developer' as dev;
 
 void main() {
+  Logger.root.level = kDebugMode ? Level.FINE : Level.INFO;
+  Logger.root.onRecord.listen((record) {
+    dev.log(
+      record.message,
+      time: record.time,
+      level: record.level.value,
+      name: record.loggerName,
+    );
+  });
   runApp(const FluentEpub());
 }
 
