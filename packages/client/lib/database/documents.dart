f36f176bfdd94fba11cdbee6493ce5590a486c85
Kiri <cultivatingghoul@gmail.com>
2026-05-23 14:45:23 +0200

    moved documents into own file

---
+++ packages/client/lib/database/documents.dart
@@ -0,0 +1,20 @@
+import 'package:drift/drift.dart';
+
+class Documents extends Table {
+  IntColumn get id => integer().autoIncrement()();
+
+  TextColumn get title => text().withLength(min: 1, max: 150)();
+  TextColumn get coverUrl => text().nullable()(); 
+  TextColumn get author => text().nullable()();
+
+  TextColumn get language => text().nullable()();
+  TextColumn get series => text().nullable()();
+  TextColumn get tags => text().nullable()();
+  
+  TextColumn get publisher => text().nullable()();
+  TextColumn get country => text().nullable()();
+
+  BoolColumn get rtl => boolean().withDefault(const Constant(false))();
+  
+  DateTimeColumn get lastModified => dateTime().withDefault(currentDateAndTime)();
+}
